88ef16cc81bd50b797fa3f6bae2b7927236096f1
EdgarMTdev <edgarmontiel961@gmail.com>
2022-04-16 22:41:49 -0500

    sockets

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const path = require("path")
-const { Server } = require("socket.io")
-const io = new Server(server)
+const path = require("path");
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get("/", (req, res) => { 
-     return res.sendFile(__dirname + "/index.html")
+app.get("/", (req, res) => {
+  return res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
-     console.log("a user connected");
-     socket.on("disconnect", () => {
-          console.log("user disconnected");
-     })
-})
+  console.log("a user connected");
+  socket.on("chat message", (message) => {
+    console.log(`message: ${message}`);
+  });
+});
+
+// io.emit("some event", {somePropety: "some value", otherPropety: "other value"});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
 
 server.listen(4000, () => {
   console.log("Running in http://localhost:4000");
